b15158be06eca2ee1cafc79a1565efc549c547d6
person12306464 <person123.06464@gmail.com>
2024-10-17 13:32:10 +0200

    changed to work with bloxstrap 2.8.0

--- bloxshade/installer/src/main.cpp
+++ bloxshade/installer/src/main.cpp
@@ -322,7 +322,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     // comment out for output
     output();
     // installer version
-    std::cout << "* Version: 2.8.15\n";
+    std::cout << "* Version: 2.9.0-bloxstrap-fix\n";
     std::cout << "* Bloxshade Installer (developed by Extravi, https://extravi.dev/)\n";
     std::cout << "* Copyright © 2024 Extravi\n";
     std::cout << "* Source Code: https://github.com/Extravi/Bloxshade\n";
@@ -396,8 +396,23 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     std::string path = robloxPath.substr(start, end - start);
     std::string roPath = robloxPath.substr(start, end - start);
 
-    // check the path
+
+    // Remove the new arg garbage thing that apparently didn't exist before
+    std::string argGarbageThing = "\" -player \"%1";
+
+    if (path.find(argGarbageThing) != std::string::npos) {
+        int startPosition = path.find(argGarbageThing);
+        path.erase(startPosition, argGarbageThing.length());
+
+        int roPathStartPosition = roPath.find(argGarbageThing);
+        roPath.erase(roPathStartPosition, argGarbageThing.length());
+
+        std::cout << "Removed arg garbage thing" << std::endl;
+    }
+
     std::cout << "Path to check: " << path << std::endl; // show the path
+
+    // check the path
     if (path.find("C:\\Program Files") == 0 || path.find("C:\\Program Files (x86)") == 0) {
         std::cout << "Program files is true" << std::endl;
         MessageBox(NULL, L"It seems like Roblox is installed system-wide in the Program Files directory. Please install Roblox in a location other than the Program Files directory.", L"Information", MB_OK | MB_ICONWARNING);
@@ -441,6 +456,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     if (bloxstrap) {
         std::cout << "Bloxstrap is true" << std::endl;
 
+        /*
         // read json
         std::ifstream file(path);
         if (!file.is_open()) {
@@ -473,13 +489,20 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
         else {
             versionGuid = data["VersionGuid"];
         }
-        std::string versionsDir = "Versions\\";
-        path.replace(BloxstrapPos, strlen("State.json"), versionsDir + versionGuid);
+        */
+
+        std::string versionsDir = "Roblox\\Player";
+        path.replace(BloxstrapPos, strlen("State.json"), versionsDir);
 
         bloxstrapPath = path + "\\RobloxPlayerBeta.exe";
+        std::cout << bloxstrapPath << std::endl;
         // defaultPath is the path to the Roblox folder
         defaultPath = path;
+
+        std::cout << "defualt path: " << defaultPath << std::endl;
+
         path = path + "\\eurotrucks2.exe";
+        std::cout << path << std::endl;
     }
     else {
         std::cout << "Bloxstrap is false" << std::endl;
